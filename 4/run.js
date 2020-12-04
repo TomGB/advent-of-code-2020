7a72eb84f679fc005fafd9f5ce6f639cce04a405
TomGB <tgbanister@gmail.com>
2020-12-04 11:16:46 +0000

    cleanup

--- 4/run.js
+++ 4/run.js
@@ -2,57 +2,39 @@ const fs = require('fs')
 
 const passportsText = fs.readFileSync('4/input.txt', 'utf8').split(`\n\n`)
 
-const passportFields = passportsText.map(passport => passport.split(/[\n\s]+/))
+const passportEntries = passportsText.map(passport => {
+    const entries = passport.split(/[\n\s]+/)
+    return entries.map(entry => entry.split(':'))
+})
 
-const passportEntries = passportFields.map(passport => passport.map(entry => entry.split(':')))
+const heights = {
+    cm: h => h >= 150 && h <= 193,
+    in: h => h >= 59 && h <= 76,
+}
 
-const requiredFields = [
-    { key: 'byr', count: 4, min: 1920, max: 2002 },
-    { key: 'iyr', count: 4, min: 2010, max: 2020 },
-    { key: 'eyr', count: 4, min: 2020, max: 2030 },
-    { key: 'hgt', cmMin: 150, cmMax: 193, inMin: 59, inMax: 76 },
-    { key: 'hcl', hex: true },
-    { key: 'ecl', options: 'amb blu brn gry grn hzl oth'.split(' ') },
-    { key: 'pid', count: 9, digitsOnly: true },
-    // 'cid',
-]
-
-const checkRequirement = ({ key, count, min, max, cmMin, cmMax, inMin, inMax, hex, options, digitsOnly }, [entryKey, value]) => {
-    if (entryKey !== key) return false
-    if (count && value.length !== count) return false
-    if (min && value < min) return false
-    if (max && value > max) return false
-    if (key === 'hgt') {
-        const isCm = value.includes('cm')
-        const isIn = value.includes('in')
-
-        if (!isCm && !isIn) return false
-
-        if (isCm) {
-            const number = value.replace('cm', '')
-            if (number < cmMin || number > cmMax) return false
-        }
-
-        if (isIn) {
-            const number = value.replace('in', '')
-            if (number < inMin || number > inMax) return false
-        }
-    }
-    if (hex && !/^#[0-9a-f]{6}$/i.test(value)) return false
-    if (digitsOnly && !/^\d+$/i.test(value)) return false
-    if (options && !options.includes(value)) return false
-    return true
+const requiredFields = {
+    byr: v => v >= 1920 && v <= 2002,
+    iyr: v => v >= 2010 && v <= 2020,
+    eyr: v => v >= 2020 && v <= 2030,
+    hgt: v => {
+        const unit = v.slice(-2)
+        const heightCheck = heights[unit]
+        return (heightCheck && heightCheck(v.slice(0, -2)))
+    },
+    hcl: v => /^#[0-9a-f]{6}$/i.test(v),
+    ecl: v => 'amb blu brn gry grn hzl oth'.split(' ').includes(v),
+    pid: v => /^\d{9}$/i.test(v),
 }
 
 const partOne = passportEntries.filter(
-    passport => requiredFields.every(
-        ({ key }) => passport.find(([entryKey]) => entryKey === key)
+    passport => Object.entries(requiredFields).every(
+        ([key]) => passport.find(([entryKey, value]) => key === entryKey)
     )
 ).length
 
 const partTwo = passportEntries.filter(
-    passport => requiredFields.every(
-        requirement => passport.find(entry => checkRequirement(requirement, entry))
+    passport => Object.entries(requiredFields).every(
+        ([key, check]) => passport.find(([entryKey, value]) => key === entryKey && check(value))
     )
 ).length
 
