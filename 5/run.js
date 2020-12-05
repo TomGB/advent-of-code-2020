ddceb78f1938c672e74b5b1d23bf480d7c5dab5a
TomGB <tgbanister@gmail.com>
2020-12-05 08:44:01 +0000

    part 2

--- 5/run.js
+++ 5/run.js
@@ -3,14 +3,29 @@ const fs = require('fs')
 
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split(`\n`).filter(x => x)
 
-const seatPositions = input.map(pass => {
-    const row = parseInt(pass.slice(0, 7).split('').map(c => c === 'F' ? 0 : 1).join(''), 2)
-    const col = parseInt(pass.slice(7).split('').map(c => c === 'L' ? 0 : 1).join(''), 2)
+const binToDec = (input, one) => parseInt(input.split('').map(c => c === one ? 0 : 1).join(''), 2)
+
+const stringToSeatId = pass => {
+    const row = binToDec(pass.slice(0, 7), 'F')
+    const col = binToDec(pass.slice(7), 'L')
     return row * 8 + col
-})
+}
+
+const seatPositions = input.map(stringToSeatId)
 
 const partOne = Math.max(...seatPositions)
 
-const partTwo = 'tbd'
+seatPositions.sort((a, b) => a - b)
+
+const findGap = (arr) => {
+    for (let position = 0; position < arr.length - 1; position++) {
+        const first = arr[position]
+        const second = arr[position + 1]
+
+        if (second - first === 2) return first + 1
+    }
+}
+
+const partTwo = findGap(seatPositions)
 
 console.log({ partOne, partTwo })
