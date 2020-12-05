dda7115162cc2e252d7f2d9a59f4fa139382ad0f
TomGB <tgbanister@gmail.com>
2020-12-05 08:54:15 +0000

    clean

--- 5/run.js
+++ 5/run.js
@@ -1,31 +1,18 @@
-const { POINT_CONVERSION_COMPRESSED } = require('constants')
 const fs = require('fs')
-
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split(`\n`).filter(x => x)
 
-const binToDec = (input, one) => parseInt(input.split('').map(c => c === one ? 0 : 1).join(''), 2)
+const binToDec = (input, zero, one) => parseInt(input.replace(zero, 0).replace(one, 1), 2)
 
-const stringToSeatId = pass => {
-    const row = binToDec(pass.slice(0, 7), 'F')
-    const col = binToDec(pass.slice(7), 'L')
+const seatPositions = input.map(pass => {
+    const row = binToDec(pass.slice(0, 7), /F/g, /B/g)
+    const col = binToDec(pass.slice(7), /L/g, /R/g)
     return row * 8 + col
-}
-
-const seatPositions = input.map(stringToSeatId)
+})
 
 const partOne = Math.max(...seatPositions)
 
 seatPositions.sort((a, b) => a - b)
 
-const findGap = (arr) => {
-    for (let position = 0; position < arr.length - 1; position++) {
-        const first = arr[position]
-        const second = arr[position + 1]
-
-        if (second - first === 2) return first + 1
-    }
-}
-
-const partTwo = findGap(seatPositions)
+const partTwo = seatPositions.find((pos, i, arr) => pos - arr[i + 1] === -2) + 1
 
 console.log({ partOne, partTwo })
