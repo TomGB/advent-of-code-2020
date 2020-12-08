2ffb78aae2a4d15f8c1b38b7f33d8ff0b15cc935
TomGB <tgbanister@gmail.com>
2020-12-08 09:43:17 +0000

    part 1 and 2

--- 8/run.js
+++ 8/run.js
@@ -1,29 +1,55 @@
 const fs = require('fs')
+const clone = input => JSON.parse(JSON.stringify(input))
+
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n').filter(x => x)
 
-const program = input.map(line => {
+const originalProgram = input.map((line, index) => {
     const [op, val] = line.split(' ')
-    return { op, val: parseInt(val) }
+    return { op, val: parseInt(val), line: index }
 })
 
-let head = 0
-let acc = 0
+const runProgram = program => {
+    let head = 0
+    let acc = 0
 
-const visitedLines = []
+    const visitedLines = []
 
-const logic = {
-    nop: () => head++,
-    acc: val => { acc += val; head++ },
-    jmp: val => head += val
+    const logic = {
+        nop: () => head++,
+        acc: val => { acc += val; head++ },
+        jmp: val => head += val
+    }
+
+    do {
+        visitedLines.push(head)
+        const { op, val } = program[head]
+
+        logic[op](val)
+
+        if (head >= program.length) return { state: 'end', acc }
+    } while (!visitedLines.includes(head))
+
+    return { state: 'loop', acc }
 }
 
-do {
-    visitedLines.push(head)
-    const { op, val } = program[head]
+const partOne = runProgram(originalProgram).acc
 
-    logic[op](val)
-} while (!visitedLines.includes(head))
+// Part Two
 
-console.log({
-    partOne: acc
-})
+const nops = originalProgram.filter(({ op }) => op === 'nop')
+const jmps = originalProgram.filter(({ op }) => op === 'jmp')
+
+const modifyLine = (originalProgram, newOp) => ({ line }) => {
+    const newProgram = clone(originalProgram)
+    newProgram[line].op = newOp
+    return newProgram
+}
+
+const newPrograms = [
+    ...nops.map(modifyLine(originalProgram, 'jmp')),
+    ...jmps.map(modifyLine(originalProgram, 'nop')),
+]
+
+const partTwo = newPrograms.map(program => runProgram(program)).find(({ state }) => state === 'end').acc
+
+console.log({ partOne, partTwo })
