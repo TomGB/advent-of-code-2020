b655885aa50637ecc8fb8be30ad2b5811e59079e
TomGB <tgbanister@gmail.com>
2020-12-15 15:55:50 +0000

    clean

--- 15/partTwo.js
+++ 15/partTwo.js
@@ -1,33 +1,22 @@
 const cliProgress = require('cli-progress')
 
 const partTwo = input => {
-    const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
-    bar1.start(30000000, 0)
-
-    let series = input.split(',').map(x => parseInt(x))
-    let turn = series.length - 1
-
-    const lastSeen = new Map()
+    const series = input.split(',').map(x => parseInt(x))
 
     let next = series.pop()
+    let turn = series.length
 
-    series.forEach((v, i) => lastSeen.set(v, i))
+    const lastSeen = new Map(series.map((v, i) => [v, i]))
 
     do {
         const index = lastSeen.get(next)
-        // console.log({ next, index, lastSeen, series })
 
         lastSeen.set(next, turn)
 
-        if (index === undefined) next = 0
-        else next = turn - index
+        next = isNaN(index) ? 0 : turn - index
 
         turn++
-
-        if (turn % 1000 === 0) bar1.update(turn)
     } while (turn < 30000000 - 1)
-
-    bar1.stop()
 
     return next
 }
