28f29fb5935e79e24cdc1d3982d1baec88a053ae
TomGB <tgbanister@gmail.com>
2020-12-15 15:08:03 +0000

    15 1 and 2

--- 15/partOne.js
+++ 15/partOne.js
@@ -1,10 +1,18 @@
 const partOne = input => {
-    const starting = input.split(',').map(x => parseInt(x))
+    let [next, ...series] = input.split(',').map(x => parseInt(x)).reverse()
+    let turn = series.length
 
-    console.log(starting)
+    do {
+        const index = series.findIndex(x => x === next)
+        series.unshift(next)
 
+        if (index === -1) next = 0
+        else next = index + 1
 
-    console.log('436')
+        turn++
+    } while (turn < 2020)
+
+    return series[0]
 }
 
 module.exports = partOne
--- 15/partTwo.js
+++ 15/partTwo.js
@@ -1,5 +1,35 @@
-const partTwo = input => {
+const cliProgress = require('cli-progress')
 
+const partTwo = input => {
+    const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+    bar1.start(30000000, 0)
+
+    let series = input.split(',').map(x => parseInt(x))
+    let turn = series.length - 1
+
+    const lastSeen = new Map()
+
+    let next = series.pop()
+
+    series.forEach((v, i) => lastSeen.set(v, i))
+
+    do {
+        const index = lastSeen.get(next)
+        // console.log({ next, index, lastSeen, series })
+
+        lastSeen.set(next, turn)
+
+        if (index === undefined) next = 0
+        else next = turn - index
+
+        turn++
+
+        if (turn % 1000 === 0) bar1.update(turn)
+    } while (turn < 30000000 - 1)
+
+    bar1.stop()
+
+    return next
 }
 
 module.exports = partTwo
